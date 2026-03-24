ce47d1d8000f8fba141589f7619d49b72cb27dd3
nova <257472@edu.p.lodz.pl>
2026-03-25 00:27:50 +0100

    Final assignment, and further constant extractions

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,28 +7,34 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
-    public static void main(String[] args) {
+    private static final int RANGE_OF_RANDOM_NUMBER = 20;
+	private static final int FIRST_RHYMER = 0;
+	private static final int RHYMERS_AMOUNT = 3;
+	private static final int COUNT_END = 15;
+	private static final int COUNT_BEGIN = 1;
+
+	public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
         
         defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
         
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = COUNT_BEGIN; i < COUNT_END; i++)
+            for (int j = FIRST_RHYMER; j < RHYMERS_AMOUNT; j++)
                 rhymers[j].countIn(i);
         
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = COUNT_BEGIN; i < COUNT_END; i++)
+            rhymers[RHYMERS_AMOUNT].countIn(rn.nextInt(RANGE_OF_RANDOM_NUMBER));
         
-        for (int i = 0; i < rhymers.length; i++) {
+        for (int i = FIRST_RHYMER; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
         
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[RHYMERS_AMOUNT]).reportRejected());
         
     }
     
